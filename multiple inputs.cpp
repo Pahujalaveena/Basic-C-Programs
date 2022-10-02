510d3be5920d5c9df346acc8b87c36906eba9961
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-02 15:13:02 +0100

    Add files via upload

---
+++ multiple inputs.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"Enter three numbers"<<endl;
+	cin>>a>>b>>c;
+	cout<<"The three numbers are:"<<a<<" "<<b<<" "<<c<<endl;
+	return 0;
+}
