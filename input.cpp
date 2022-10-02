f9c8725c446d7eb2fcc95ca7ce25d74d8258bc41
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-02 15:12:21 +0100

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter your age:";
+	cin>>age;    //"cin>> is used to take input from user"
+	cout<<"Your age is :"<<age<<endl;
+	return 0;
+}
