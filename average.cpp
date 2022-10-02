7cd726b5ea1e45063f1be586f4bfe6da8da58de6
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-02 15:10:27 +0100

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e;
+	float avg;
+	cout<<"Enter the numbers:"<<endl;
+	cin>>a>>b>>c>>d>>e;
+	avg=(float)(a+b+c+d+e)/5;
+	cout<<"The average is :"<<avg<<endl;
+	return 0;
+	
+}
