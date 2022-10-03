7eca8e732e85ef2a4a51c38c7a0f4b4509b76cbf
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-03 11:21:35 +0100

    Add files via upload

---
+++ odd&even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"ODD amd EVEN!!"<<endl;
+	cout<<"Enter the number:"<<endl;
+	cin>>n;
+	if(n%2==0)
+	cout<<"Number is even!"<<endl;
+	else
+	cout<<"Number s not even!"<<endl;
+	return 0;
+}
