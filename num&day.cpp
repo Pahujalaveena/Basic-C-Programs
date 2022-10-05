642c5abb5a6f847d35fdb0f714bb648dae49779c
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-05 11:59:54 +0100

    Add files via upload

---
+++ num&day.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter the number:"<<endl;
+	cin>>num;
+	cout<<"The day is:"<<endl;
+	switch(num)
+	{
+		case 1:
+			{
+				cout<<"Monday!"<<endl;
+				break;
+			}
+		case 2:
+			{
+				cout<<"Tuesday!"<<endl;
+				break;
+			}
+		case 3:
+			{
+				cout<<"Wednesday!"<<endl;
+				break;
+			}
+		case 4:
+			{
+				cout<<"Thursday!"<<endl;
+				break;
+			}
+		case 5:
+			{
+				cout<<"Friday!"<<endl;
+				break;
+			}
+		case 6:
+			{
+				cout<<"Saturday!"<<endl;
+				break;
+			}
+		case 7:
+			{
+				cout<<"Sunday!"<<endl;
+				break;
+			}
+		default:
+			{
+				cout<<"Invalid Input!"<<endl;
+			}
+		
+	}
+	return 0;
+}
