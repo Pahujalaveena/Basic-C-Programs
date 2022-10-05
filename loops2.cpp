1fe975a944f0fb09965b2f663853cc335cd92173
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-05 11:56:56 +0100

    Add files via upload

---
+++ loops2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter the number!!"<<endl;
+	cin>>num;
+	for(int i=num;i>=1;i--)
+	{
+		cout<<i<<endl;
+	}
+}
