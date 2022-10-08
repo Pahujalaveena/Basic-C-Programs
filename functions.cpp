f5edd08c1dddd63eb265795a47f9a83c75aff3be
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-10-08 20:23:27 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void msg()
+{
+	cout<<"Hello World!!"<<endl;
+}
+void msg1()
+{
+	cout<<"Welcome!!"<<endl;
+}
+int main()
+{
+	msg();
+	msg1();
+	msg();
+	msg1();
+}
